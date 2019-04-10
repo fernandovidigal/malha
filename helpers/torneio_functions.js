15995f3e3bb880c4d9867a9b624aa9a3318917ef
Fernando <fernandovidigal@gmail.com>
2019-04-10 17:27:59 +0100

    Continuação da implementação da distribuição das equipas pelos campos

--- helpers/torneio_functions.js
+++ helpers/torneio_functions.js
@@ -15,7 +15,60 @@ function determinaNumeroTotalCampos(numEquipas, numCampos, minCampos, maxCampos)
 }
 
 function verificaLocalidade(campo, localidade){
+    return false;
+}
+
+function verificaCamposDisponiveis(listaCampos, localidade_id, maxEquipasPorCampo, numeroMinimoEquipasCampo = -1){
+    let camposDisponiveis = new Array();
+    let contemLocalidade = false;
+
+    for(i = 0; i < listaCampos.length; i++){
+        contemLocalidade = false;
+
+        if(listaCampos[i].length < maxEquipasPorCampo){
+
+            // Foi definido um número minimo de equipas por campo
+            if(numeroMinimoEquipasCampo != -1){
+                if(listaCampos[i].length == numeroMinimoEquipasCampo){
+                    for(j = 0; j < listaCampos[i].length; j++){
+                        if(listaCampos[i][j].localidade_id == localidade_id){
+                            contemLocalidade = true;
+                            break;
+                        }
+                    }
+        
+                    if(!contemLocalidade){
+                        camposDisponiveis.push(i);
+                    }
+                }
+            } else { // Não foi definido o número minimo de equipas por campo
+                for(j = 0; j < listaCampos[i].length; j++){
+                    if(listaCampos[i][j].localidade_id == localidade_id){
+                        contemLocalidade = true;
+                        break;
+                    }
+                }
     
+                if(!contemLocalidade){
+                    camposDisponiveis.push(i);
+                }
+            }
+        }
+    }
+    return camposDisponiveis;
+}
+
+function verificaMinimoEquipasPorCampo(listaCampos, numeroMinimoEquipasCampo){
+
+    for(i = 0; i < listaCampos.length; i++){
+        // Existe pelo menos um campo com o mínimo de equipas actual
+        if(listaCampos[i].length == numeroMinimoEquipasCampo){
+            console.log("Existe campo com o nínimo de equipas");
+            return numeroMinimoEquipasCampo;
+        }
+    }
+
+    return (numeroMinimoEquipasCampo + 1);
 }
 
 // Por cada escalão
@@ -27,37 +80,64 @@ function verificaLocalidade(campo, localidade){
 async function distribuir(malhaDB, equipas, escalao, numCampos, minCampos, maxCampos){
     return new Promise(function(resolve, reject){
         
+        const numeroEquipas = equipas.length;
         const totalCampos = determinaNumeroTotalCampos(equipas.length, numCampos, minCampos, maxCampos);
-        console.log("Total Campos: " + totalCampos);
         if(totalCampos == 0){
             return reject("Número de campos insuficiente.");
         }
 
+        const maxEquipasPorCampo = Math.ceil(numeroEquipas / totalCampos);
+
+        const xpto = verificaLocalidade(1, 2);
+
         // Inicia a Array de campos
-        listaCampos = [];
+        let listaCampos = [];
         for(i = 0; i < totalCampos; i++){
-            listaCampos.push([]);
+            listaCampos.push(new Array());
         }
 
-        i = 0;
+        let numeroMinimoEquipasCampo = 0;
+        
         while(equipas.length > 0){
-            if(i >= totalCampos) {
-                i = 0;
-            }
+            console.log("Número de Equipas: " + equipas.length);
+
+            // Obtem uma equipa aleatória
             const equipaRandom = Math.floor(Math.random() * equipas.length);
-            console.log("Length: " + equipas.length);
-            console.log("equipaRandom: " + equipaRandom);
-            console.log("Equipa["+equipaRandom+"] = " + equipas[equipaRandom]);
-            listaCampos[i].push(equipas[equipaRandom]);
-            equipas.splice(equipaRandom, 1);
-            
+            let equipa = equipas[equipaRandom];
+            console.log(equipa);
+            console.log(" ");
 
-            i++;
+            // Actualiza o número mínimo de equipas que existe em cada campo
+            numeroMinimoEquipasCampo = verificaMinimoEquipasPorCampo(listaCampos, numeroMinimoEquipasCampo);
+            console.log("Numero minino de equipas: " + numeroMinimoEquipasCampo);
+
+            // Verifica o número de campos disponiveis para alocar a equipa
+            let camposDisponiveis = verificaCamposDisponiveis(listaCampos, equipa.localidade_id, maxEquipasPorCampo, numeroMinimoEquipasCampo);
+            console.log("Localidade: "+equipa.localidade_id);
+            console.log("Campos Disponíveis: "+camposDisponiveis);
+
+            if(camposDisponiveis.length != 0){
+                console.log("Aqui");
+                listaCampos[camposDisponiveis[0]].push(equipa);
+                equipas.splice(equipaRandom, 1);
+            } else {
+                camposDisponiveis = verificaCamposDisponiveis(listaCampos, equipa.localidade_id, maxEquipasPorCampo);
+                console.log("Campos Disponiveis2: " + camposDisponiveis);
+                listaCampos[camposDisponiveis[0]].push(equipa);
+                equipas.splice(equipaRandom, 1);
+            }
+
+            console.log(listaCampos);
         }
-
-        console.log(listaCampos);
     });
 }
+
+
+
+
+
+
+
 
 module.exports.torneio_functions = {
 
@@ -67,20 +147,24 @@ module.exports.torneio_functions = {
         // Número de campos
         let numCampos = await malhaDB.torneios.getNumCampos(torneio_id);
         numCampos = numCampos.campos;
-        //console.log(numCampos);
 
         // Todos os escalões que têm equipas
         const listaEscaloes = await malhaDB.equipas.getAllEscaloesWithEquipa(torneio_id);
         let escaloes = Array.from(listaEscaloes, escalao => escalao.escalao_id);
-        //console.log(escaloes);
 
         // Número de equipas por escalão
         const numEquipasPorEscalao = await malhaDB.equipas.getNumEquipasPorEscalao(torneio_id);
-        //console.log(numEquipasPorEscalao);
 
         // ***********
         const listaEquipas = await malhaDB.equipas.getAllEquipaIDAndLocalidade(torneio_id, 1);
-        let equipas = Array.from(listaEquipas, equipa => [equipa.equipa_id, equipa.localidade_id]);
+        let equipas = Array.from(listaEquipas, equipa => {
+            let data = {
+                "equipa_id":equipa.equipa_id,
+                "localidade_id":equipa.localidade_id
+            };
+
+            return data;
+        });
 
         await distribuir(malhaDB, equipas, 1, numCampos, minCampos, maxCampos)
             .catch((err) => {
@@ -108,3 +192,21 @@ module.exports.torneio_functions = {
         });*/
     }
 }
+
+
+
+/*i = 0;
+while(numeroEquipas > 0){
+    if(i >= totalCampos) {
+        i = 0;
+    }
+    const equipaRandom = Math.floor(Math.random() * numeroEquipas);
+    console.log("Localidade: " + equipas[equipaRandom].localidade_id);
+    listaCampos[i].push(equipas[equipaRandom]);
+    equipas.splice(equipaRandom, 1);
+    
+
+    i++;
+}
+
+console.log(listaCampos);*/
